7dcab426e0b5106f6702fed5261ed2b89f2ae39b
knor12 <32679571+knor12@users.noreply.github.com>
2023-04-19 15:05:02 +0100

    Add files via upload

--- CanPacker/code/Utilities.py
+++ CanPacker/code/Utilities.py
@@ -9,7 +9,7 @@ class Utilities:
     def __init__(self, name=""):
         self.name = name
         self.toolName = "CanPacker"
-        self.version = "V0.04"
+        self.version = "V0.05"
 
  
     
